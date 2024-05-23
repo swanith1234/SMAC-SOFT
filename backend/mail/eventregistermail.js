7c1bea14de69dc710d28598d3b396b1c2a7e7c22
swanith1234 <swanithpidugu@gmail.com>
2024-05-23 19:21:27 +0530

    configured secrets

--- backend/mail/eventregistermail.js
+++ backend/mail/eventregistermail.js
@@ -4,62 +4,14 @@ import { Application } from "../models/application.js";
 import { Employer } from "../models/employer.js";
 import { google } from "googleapis";
 import jwt from "jsonwebtoken";
-const CLIENT_ID =
-  "322077792958-jh3aqn30o4n6k3dc7gst0l7jq97bn1k3.apps.googleusercontent.com";
-const CLIENT_SECRET = "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";
-const REDIRECT_URL = "https://developers.google.com/oauthplayground";
-const REFRESH_TOKEN =
-  "1//04gNIBOs5JxiWCgYIARAAGAQSNwF-L9IrIAenypCNihXcLmDMTvw1r_8NyNSH0SP-mtTnKy-NClwmDwOmNRJw02kjQUMi_KtsjgA";
+import "dotenv";
 
 const oAuth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URL
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URL
 );
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-
-// const sendEmail = async (req, res) => {
-//   console.log("request");
-//   console.log(req.params);
-
-//   try {
-//     const clickedUser = await User.findOne({ email: req.params.id });
-
-//     let dbusers = await User.find({ email: { $ne: req.params.id } });
-//     console.log(dbusers);
-
-//     let mailTransporter = nodemailer.createTransport({
-//       service: "gmail",
-//       auth: {
-//         type: "OAuth2",
-//         user: "swanithpidugu@gmail.com",
-//         CLIENT_ID,
-//         CLIENT_SECRET,
-//         refreshToken: REFRESH_TOKEN,
-//         accessToken: oAuth2Client.getAccessToken(),
-//       },
-//     });
-
-//     await Promise.all(
-//       dbusers.map(async (user) => {
-//         let mailDetails = {
-//           from: "mushumohammad61@gmail.com",
-//           to: "swanithpidugu@gmail.com",
-//           subject: "Test mail",
-//           text: `Hello swanith, how are you?`,
-//         };
-
-//         await mailTransporter.sendMail(mailDetails);
-//         console.log("Email sent successfully");
-//       })
-//     );
-
-//     res.status(200).send("Emails sent successfully");
-//   } catch (error) {
-//     console.error("Error occurred:", error);
-//     res.status(500).send("Internal Server Error");
-//   }
-// };
 
 const sendEmailSelect = async (req, res) => {
   console.log("request");
