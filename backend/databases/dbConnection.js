6ecf896bf3acd915852a0e76505fd7785218984f
swanith1234 <swanithpidugu@gmail.com>
2024-05-23 21:44:18 +0530

    configured db name

--- backend/databases/dbConnection.js
+++ backend/databases/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 export const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URL, {
-      dbname: "SMAC_SOFT",
+      dbname: process.env.dbname,
     })
     .then(() => {
       console.log("connected to database");
