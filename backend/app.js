5377410148d52830f0f919aa4e30fea3ed3581dc
swanith1234 <swanithpidugu@gmail.com>
2024-06-15 14:49:34 +0530

    Initial commit

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5174", // Allow requests from this origin
+    origin: "http://localhost:5173", // Allow requests from this origin
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
   })
 );
